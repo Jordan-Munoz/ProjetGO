27c86c9931a9b9d8aef78719f85fd9bcd31317ee
Jordan Munoz <munoz.jordan38@hotmail.com>
2023-12-19 17:22:49 +0100

    Ajout des workers

--- main.go
+++ main.go
@@ -9,3 +9,24 @@ import (
 	"sync"
 	"time"
 )
+
+//By Jordan Munoz for ProjectGO 
+//Worker = les travailleurs :D
+func Worker(target string, dictionary chan string, results chan string) {
+	for word := range dictionary {
+		// URL en combinant cible et la liste sinon marche pas
+		url := fmt.Sprintf("%s/%s", target, word)
+		
+		// Tester la connexion
+		response, err := http.Get(url)
+		
+		// Verif si la connexion passe ou pas (200 OK - 404 Pas OK)
+		if err == nil {
+			// Envoie du résultat
+			results <- fmt.Sprintf("%s %d", url, response.StatusCode)
+		} else {
+			// Si échoue = 404
+			results <- fmt.Sprintf("%s 404", url)
+		}
+	}
+}
